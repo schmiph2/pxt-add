3187395eee26336d24ce38a3f093a1029a910b89
schmiph2 <43240826+schmiph2@users.noreply.github.com>
2019-02-12 15:33:29 +0100

    Update main.ts

--- main.ts
+++ main.ts
@@ -3,7 +3,7 @@
 // MIT License
 // Changes for Calliope Mini by M. Klein
 //
-//% weight=0 color=#87bc4b icon="\uf1eb" block="Infrarot IR"
+//% weight=0 color=#87bc4b icon="\uf1eb" block="Test 123"
 namespace IR {
     export enum encodingType {
         //% block="NEC"
